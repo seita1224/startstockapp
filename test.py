060ef19fdd31c4d06e01d2ccf7c5313bd0d7f53b
Yuki Okamoto <okamoto_yu-ki@outlook.jp>
2018-10-24 23:24:06 +0900

    更新

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def login():
         </form>
     '''.format(username=username, password=password)
 
-#a
+#a#a
 @route('/login', method='POST')  # or @post('/post')
 def do_login():
     username = request.forms.get('username')
