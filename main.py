ed12f8b6bd4897b9a83ffe81471a250f5241f561
seita <seita.1224.ds@outlook.com>
2018-11-01 21:42:50 +0900

    環境設定

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from bottle import get, post, request,redirect
 
 @route('/', method='GET')
 def defult():
-    test = "test"
-    test = 12345
 
-    return test
+    return "StartStockApp"
 
 
 run(host='0.0.0.0', port=8080)
