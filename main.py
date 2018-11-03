51c82e318b96f5bdc9156264d9dab836f137bc61
seita <seita.1224.ds@outlook.com>
2018-11-03 13:04:01 +0900

    webサーバ起動テスト追加設定

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from bottle import Bottle, route, run, template
+from bottle import Bottle, route, run, template,default_app
 # todo bottleの設定の一元管理構想設定クラス(作成中)
 from config.config import Setting
 import bottle
 
 
-app = Bottle()
 # todo bottleの設定の一元管理構想(作成中)
-#app = Setting(bottle)
+# app = Setting(bottle)
 file_main_path = "/static/"
 # 静的ファイルパス設定
 bottle.TEMPLATE_PATH += ["./" + file_main_path + "/html"]
@@ -14,12 +13,13 @@ bottle.TEMPLATE_PATH += ["./" + file_main_path + "/css"]
 bottle.TEMPLATE_PATH += ["./" + file_main_path + "/js"]
 
 
-
-@app.route('/', method='GET')
+@route('/', method='GET')
 def defult():
 
     return template("index")
 
 
 if __name__ == "__main__":
-    run(app=app,host="0.0.0.0", quiet=False, reloader=True)
+    run(host="0.0.0.0", quiet=False, reloader=True)
+else:
+    application = default_app()
